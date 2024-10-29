83fe3f40bd60330a7032d16783beddbd0d1e5353
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-29 17:27:33 +0100

    guitars_table_done

--- resources/views/guitars/index.blade.php
+++ resources/views/guitars/index.blade.php
@@ -3,36 +3,65 @@
 @section("page-title")
 
 @section("main-content")
-<h1>Guitars:</h1>
+<h1>Guitars List:</h1>
+<div class="container">
 
-<table class="table table-dark">
+<div class="row">
+    <div class="col-12">
+        <div class="mb-3">
+            <a href="#" class="btn btn-sm btn-primary me-1 m-1">Create new Guitar!</a>
+
+        </div>
+    
+<table class="table table-dark"> 
+    {{--? "title","description","thumb","price","series","release_date","type","genre","artist"--}}
     <thead>
       <tr>
         <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Descrizione</th>
+        <th scope="col">Url_Immagine</th>
+        <th scope="col">Prezzo</th>
+        <th scope="col">Serie</th>
+        <th scope="col">Data rilascio</th>
+        <th scope="col">Tipo</th>
+        <th scope="col">Genere</th>
+        <th scope="col">Artista</th>
+        <th scope="col">Azioni</th>
       </tr>
     </thead>
     <tbody>
+        @forelse ($guitars as $index => $guitar)
+            
+       
       <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
+        <td>{{$guitar->id}}</td>
+        <td>{{$guitar->title}}</td>
+        <td>{{$guitar->description}}</td>
+        <td>{{$guitar->thumb}}</td>
+        <td>{{$guitar->price}}</td>
+        <td>{{$guitar->series}}</td>
+        <td>{{$guitar->release_date}}</td>
+        <td>{{$guitar->type}}</td>
+        <td>{{$guitar->genre}}</td>
+        <td>{{$guitar->artist}}</td>
+        <td>
+            <a href="#" class="btn btn-sm btn-primary me-1 m-1">Show</a>
+            <a href="#" class="btn btn-sm btn-success me-1">Edit</a>
+            <a href="#" class="btn btn-sm btn-warning me-1">Delete</a>
+          </td>
       </tr>
+    
+      @empty
       <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
+        <td colspan="11">Non ci sono Chitarre disponibili al momento..
+        </td>
       </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-      </tr>
+            
+      @endforelse
     </tbody>
   </table>
+</div>
+</div>
+</div>
 @endsection
